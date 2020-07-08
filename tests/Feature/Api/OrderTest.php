c40a3667e6b5e5c71d5785d427c689a141b1dad1
Mateus Lima de Freitas <mateus_lima_99@hotmail.com>
2020-07-07 21:47:57 -0300

    Teste de pedidos codificada

--- tests/Feature/Api/OrderTest.php
+++ tests/Feature/Api/OrderTest.php
@@ -3,8 +3,12 @@
 namespace Tests\Feature\Api;
 
 use Tests\TestCase;
+use App\Models\Order;
+use App\Models\Table;
+use App\Models\Client;
 use App\Models\Tenant;
 use App\Models\Product;
+use Illuminate\Support\Str;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
@@ -84,5 +88,124 @@ class OrderTest extends TestCase
         $response->dump();
         $response->assertStatus(201)
             ->assertJsonPath('data.total', 60);
+    }
+
+    /**
+     * Order Not Found.
+     *
+     * @return void
+     */
+    public function testOrderNotFound()
+    {
+        $order = 'fake_value';
+
+        $response = $this->getJson("/api/v1/orders/{$order}");
+
+        $response->assertStatus(404);
+    }
+
+    /**
+     * Get Order.
+     *
+     * @return void
+     */
+    public function testGetOrder()
+    {
+        $order = factory(Order::class)->create();
+
+        $response = $this->getJson("/api/v1/orders/{$order->identify}");
+
+        $response->assertStatus(200);
+    }
+
+    /**
+     * Test Create new Order Autheticated.
+     *
+     * @return void
+     */
+    public function testCreateNewOrderAutheticated()
+    {
+        $tenant = factory(Tenant::class)->create();
+        $payload = [
+            'token_company'=> $tenant->uuid,
+            'products' => []
+        ];
+
+
+        $products = factory(Product::class, 2)->create();
+        foreach($products as $product){
+            array_push($payload['products'], [
+                'identify' => $product->uuid,
+                'qty' => 2,
+            ]);
+        }
+
+        $client = factory(Client::class)->create();
+        $token = $client->createToken(Str::random(10))->plainTextToken;
+
+        $response = $this->postJson('/api/auth/v1/orders', $payload, [
+            'Authorization' => "Bearer {$token}"
+        ]);
+
+        // $response->dump();
+        $response->assertStatus(201);
+    }
+
+    
+    /**
+     * Test Create new Order With Table.
+     *
+     * @return void
+     */
+    public function testCreateNewOrderWithTable()
+    {
+        $table = factory(Table::class)->create();
+        
+        $tenant = factory(Tenant::class)->create();
+        $payload = [
+            'token_company'=> $tenant->uuid,
+            'table'=> $table->uuid,
+            'products' => []
+        ];
+
+
+        $products = factory(Product::class, 2)->create();
+        foreach($products as $product){
+            array_push($payload['products'], [
+                'identify' => $product->uuid,
+                'qty' => 2,
+            ]);
+        }
+
+        $client = factory(Client::class)->create();
+        $token = $client->createToken(Str::random(10))->plainTextToken;
+
+        $response = $this->postJson('/api/v1/orders', $payload, [
+            'Authorization' => "Bearer {$token}"
+        ]);
+
+        // $response->dump();
+        $response->assertStatus(201);
+    }
+
+    
+    /**
+     * Test Get My Orders.
+     *
+     * @return void
+     */
+    public function testGetMyOrders()
+    {
+        $client = factory(Client::class)->create();
+        $token = $client->createToken(Str::random(10))->plainTextToken;
+
+        factory(Order::class, 15)->create(['client_id' => $client->id]);
+
+        $response = $this->getJson('/api/auth/v1/my-orders', [
+            'Authorization' => "Bearer {$token}"
+        ]);
+
+        $response->dump();
+        $response->assertStatus(200);
     }
 }
